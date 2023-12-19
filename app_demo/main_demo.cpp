4944555d3bb0f59ccc4875fc9594aa42878b47b6
Andrew Byrnes <byrnes.aw@gmail.com>
2023-12-18 20:28:46 -0600

    laying out demo

--- app_demo/main_demo.cpp
+++ app_demo/main_demo.cpp
@@ -1,7 +1,49 @@
 #include "../code/AVLTree.h"
 
 int main() {
+    /*
+    Points Per Game (PPG)
+
+        This section demonstrates:
+            - Creating an AVLTree instance to track a high to low sorted stat
+            - Inserting nodes into the AVLTree in random order
+                - Displaying an error message if a duplicate node is inserted
+            - Using the Find Function to find a node by statValue
+                - demonstrating both a found node and a not found node
+            - Creating a visual GraphViz representation of the AVLTree with all nodes added
+            - Listing the stats in order with all nodes added
+            - Removing a node from the AVLTree by statValue
+            - Creating a visual GraphViz representation of the AVLTree after a node has been removed
+            - Listing the stats in order after a node has been removed
+
     
+    */ 
+
+
+
+
+
+
+   /* 
+   Turnovers Per Game (TOPG)
+
+        This section demonstrates:
+            - Creating an AVLTree instance to track a low to high sorted stat
+            - Inserting nodes into the AVLTree in random order
+            - After every insertion a visual GraphViz representation of the AVLTree is created
+            
+   */ 
+
+  /*
+  Very large AVL Tree
+
+        This section demonstrates:
+            - Mocking up the creation of a very large AVLTree
+            - Creating a visual GraphViz representation of the AVLTree periodically to demonstrate
+                the adherence to the AVLTree invariants 
+            - These trees are randomly created every time the program is run, GraphViz files
+                are created with timestamps appended by a _int to prevent overwriting
+  */
     return 0;
 }
 
